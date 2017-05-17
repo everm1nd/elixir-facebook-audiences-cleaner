66aba9593cbf8bb20900115f1f5bb0cadddeffc6
everm1nd <everm1nd.mail@gmail.com>
2017-05-17 18:36:14 +0200

    added `adsets` function to get AdSets from Facebook API

--- lib/facebook_audience_cleaner.ex
+++ lib/facebook_audience_cleaner.ex
@@ -1,6 +1,39 @@
 defmodule FacebookAudienceCleaner do
   def clean do
+    start
+    adsets.body
+    |> IO.inspect
+  end
+
+  defp start do
     HTTPotion.start
-    HTTPotion.get "httpbin.org/get"
+  end
+
+  defp adsets do
+    HTTPotion.get adsets_url(ad_account_id), query: adsets_params
+  end
+
+  defp adsets_params do
+    Map.merge(default_params, %{
+      fields: "targeting"
+    })
+  end
+
+  defp default_params do
+    %{
+      access_token: access_token
+    }
+  end
+
+  defp adsets_url(ad_account_id) do
+     "https://graph.facebook.com/v2.9/act_#{ad_account_id}/adsets"
+  end
+
+  defp ad_account_id do
+    System.get_env("AD_ACCOUNT_ID")
+  end
+
+  defp access_token do
+    System.get_env("ACCESS_TOKEN")
   end
 end
