7788cd3a717e72465b88a0112c5a8c0023d2dc94
everm1nd <everm1nd.mail@gmail.com>
2017-05-22 17:23:35 +0200

    added functionality to print audiences

--- lib/facebook_audience_cleaner.ex
+++ lib/facebook_audience_cleaner.ex
@@ -3,10 +3,22 @@ defmodule FacebookAudienceCleaner do
   require Audiences
 
   def clean do
-    AdSets.get_adsets
-    |> AdSets.extract_audiences
-    |> IO.inspect
-    Audiences.get_audiences
-    |> IO.inspect
+    audiences_in_use =
+      AdSets.get_adsets
+      |> AdSets.extract_audiences
+      |> get_ids
+      |> Enum.uniq
+    all_audiences =
+      Audiences.get_audiences
+      |> get_ids
+      |> Enum.uniq
+    IO.puts "Found #{audiences_in_use |> Enum.count} audiences in use"
+    IO.puts "Found #{all_audiences |> Enum.count} audiences total"
+    IO.puts "Printing IDs of not used audiences:"
+    Enum.each(all_audiences -- audiences_in_use, &IO.puts(&1))
+  end
+
+  def get_ids(elements) do
+    Enum.map(elements, &(&1[:id]))
   end
 end
