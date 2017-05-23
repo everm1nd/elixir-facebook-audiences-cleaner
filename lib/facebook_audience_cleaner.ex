68be8374162ba5489dadaa431b365cbf5a56ae19
everm1nd <everm1nd.mail@gmail.com>
2017-05-23 12:18:55 +0200

    modified script to print how many audiences are not in use

--- lib/facebook_audience_cleaner.ex
+++ lib/facebook_audience_cleaner.ex
@@ -12,13 +12,19 @@ defmodule FacebookAudienceCleaner do
       Audiences.get_audiences
       |> get_ids
       |> Enum.uniq
+    non_used_audiences = all_audiences -- audiences_in_use
     IO.puts "Found #{audiences_in_use |> Enum.count} audiences in use"
     IO.puts "Found #{all_audiences |> Enum.count} audiences total"
+    IO.puts "#{non_used_audiences |> Enum.count} audiences are not in use"
     IO.puts "Printing IDs of not used audiences:"
-    Enum.each(all_audiences -- audiences_in_use, &IO.puts(&1))
+    print_ids(non_used_audiences)
   end
 
   def get_ids(elements) do
     Enum.map(elements, &(&1[:id]))
+  end
+
+  def print_ids(elements) do
+    Enum.each(elements, &IO.puts(&1))
   end
 end
